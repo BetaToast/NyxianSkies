abf740fb42545c084a6bd808b11166d80b0c16e6
Xeno PuTtSs <xenoputtss@gmail.com>
2015-03-02 21:56:23 -0500

    Reduced the number of movement Messaged down to 3

--- NyxianSkies/NyxianSkies/ServerSide/GameInstance/Actions/MovementActions.cs
+++ NyxianSkies/NyxianSkies/ServerSide/GameInstance/Actions/MovementActions.cs
@@ -1,63 +1,21 @@
 ﻿using System;
+using System.Drawing;
 
 namespace NyxianSkies.ServerSide.GameInstance.Actions
 {
-    public class MoveUp : IGameAction, IPlayerAction
+    public class Move : IGameAction, IPlayerAction
     {
         public Guid GameId { get; set; }
         public Guid PlayerId { get; set; }
+        public Point Direction { get; set; }
     }
-    public class MoveUpStart : IGameAction, IPlayerAction
+    public class MoveStart : IGameAction, IPlayerAction
     {
         public Guid GameId { get; set; }
         public Guid PlayerId { get; set; }
+        public Point Direction { get; set; }
     }
-    public class MoveUpStop : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveRight : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveRightStart : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveRightStop : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveDown : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveDownStart : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveDownStop : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveLeft : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveLeftStart : IGameAction, IPlayerAction
-    {
-        public Guid GameId { get; set; }
-        public Guid PlayerId { get; set; }
-    }
-    public class MoveLeftStop : IGameAction, IPlayerAction
+    public class MoveStop : IGameAction, IPlayerAction
     {
         public Guid GameId { get; set; }
         public Guid PlayerId { get; set; }
